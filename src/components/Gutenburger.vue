5a68eb6b4d0ad924a3b3e2a27d134068d4ea9ac0
js837 <jonathanasoder@outlook.com>
2021-01-21 23:12:57 +0100

    adding frontend part of paragraph-blocks

--- src/components/Gutenburger.vue
+++ src/components/Gutenburger.vue
@@ -2,9 +2,17 @@
   <div class="gutenburger-editor">
       <Nav></Nav>
     
-      <form class="editor">
-          <textarea @keyup="resizeTitleField($event)" placeholder="Title" class="post-heading" id="post-heading" name="postHeading"></textarea>
-      </form>
+    <form class="editor">
+        <textarea @keyup="resizeTitleField($event)" placeholder="Title" class="post-heading" id="post-heading" name="postHeading"></textarea>
+
+        <div class="post-body">
+            <p class="post-body-paragraph" data-block="block-1" id="block-1" contenteditable="true" placeholder="Start Writing or type / to choose a block"></p>
+        </div>
+
+        <!-- <textarea placeholder="Content" class="post-body" id="post-body" name="postBody"></textarea>   -->
+    
+      
+    </form>
 
   </div>
 </template>
@@ -17,6 +25,7 @@ import EditorNav from './Nav';
 export default {
     data: function(){
         return {
+            placeholderIsActive: true,
             titleAreaHeight: 0,
         }
     },
@@ -36,8 +45,22 @@ export default {
 
             // then grab the height calculated from auto and apply it to the element again.
             textArea.style.height = `${textArea.scrollHeight}px`;
+        },
+        togglePlaceholderText: function(e){
 
-        }
+            // e.currentTarget.focus;
+            console.log(e.currentTarget);
+
+            document.getElementById('block-1').focus();
+             document.getElementById('block-1').focus();
+
+            if(this.placeholderIsActive){
+                this.placeholderIsActive = false;
+            }else if(!this.placeholderIsActive && e.target.innerText.length == 0){
+                this.placeholderIsActive = true;
+            }
+        },
+
     }
 }
 </script>
@@ -49,12 +72,28 @@ export default {
     /* Import some text styling */
     @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap');
 
+    $btn-text-dark-text: #161616;
+
     .gutenburger-editor {
         width: 100%;
         min-height: 100vh;
         box-sizing: border-box;
         display: flex;
         flex-direction: column;
+        font-family: 'Open Sans', sans-serif;
+        font-weight: 400;
+        
+        // Make sure placeholders for contenteditable elements are visible
+        [contenteditable][placeholder]:empty:before {
+            content: attr(placeholder);
+            color: lighten($color: $btn-text-dark-text, $amount: 50);
+        }
+
+        // Remove outlines of contenteditable element placeholders on focus
+        [contenteditable][placeholder]:focus {
+            outline: none;
+            color: inherit;
+        }
 
         .editor {
             align-self: center;
@@ -72,10 +111,41 @@ export default {
                 overflow: hidden;
                 resize: none;
                 box-sizing: border-box;
+                color: lighten($color: $btn-text-dark-text, $amount: 50);
+                font-family: inherit;
 
                 &:focus {
                     border: none;
                     outline: 2px solid transparent;
+                    color: $btn-text-dark-text;
+                }
+            }
+
+            .post-body {
+                width: 100%;
+                font-size: 1rem;
+                font-weight: 400;
+                border: none;
+                overflow: hidden;
+                resize: none;
+                box-sizing: border-box;
+                color: lighten($color: $btn-text-dark-text, $amount: 50);
+                font-family: inherit;
+
+                // Show content
+                &:focus {
+                    border: none;
+                    outline: none;
+                    color: $btn-text-dark-text;
+                }
+
+                .post-body-paragraph {
+                    padding: 5px 5px; 
+                    box-sizing: border-box;
+                    cursor: text;
+                    color: $btn-text-dark-text;
+                    font-size: 1.125rem;
+                    line-height: 1.75rem;
                 }
             }
         }
